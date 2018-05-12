a6922ccef091fac30cebaad63e5ce6c80be8d563
JuseungLee <juseung@fusic.co.jp>
2018-05-12 18:28:41 +0900

    :sparkles: デザイン修正

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -72,7 +72,7 @@ const theme = createMuiTheme({
       fbCommentsColorscheme: "light"
     },
     sizes: {
-      articleMaxWidth: "50em"
+      articleMaxWidth: "65em"
     },
     fonts: {
       title: {
